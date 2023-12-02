16d8fd9de5b9a1b1aca98f62d7fbed2b1af19697
Creator <stefano.coletta@mongodb.com>
2023-12-02 18:16:01 +0100

    Day 2 solutions

---
+++ aoc2023/day2/code1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+AoC 2023 day 2 - Solution 1/2
+"""
+
+R_CUBES = 12
+G_CUBES = 13
+B_CUBES = 14
+
+tot = 0 # pylint: disable=invalid-name
+game_counter = 0 # pylint: disable=invalid-name
+
+def parse(content):
+    """
+    Parses a string containing color quantity information and returns the maximum quantity
+    for each of the three primary colors: red, green, and blue.
+
+    Parameters:
+    - content (str): A string containing color quantity information separated by semicolons
+                    and color-quantity pairs separated by commas. Each pair consists of a
+                    quantity and a color name.
+
+    Returns:
+    tuple: A tuple containing the maximum quantity for red, green, and blue colors, respectively.
+
+    Example:
+    >>> parse("2 red, 5 green; 3 red, 7 blue; 1 green, 4 blue")
+    (3, 7, 4)
+
+    Note:
+    - The function assumes a specific format for the input string, where colors are named
+      ('red', 'green', 'blue') and quantities are integers.
+    - If the input format does not adhere to the expected structure, the function behavior
+      may not be accurate.
+    """
+    color_max = { 'red': 0, 'green': 0, 'blue': 0 }
+    grabs = content.split(';')
+    for grab in grabs:
+        subsets = grab.split(',')
+        for subset in subsets:
+            subset = subset.strip()
+            (qty, color) = subset.split(' ', 2)
+            if color_max[color] < int(qty):
+                color_max[color] = int(qty)
+    return (color_max['red'], color_max['green'], color_max['blue'])
+
+with open("aoc2023/day2/input.txt", encoding="utf-8") as file:
+    for line in file:
+        game_counter += 1
+        line = line.replace("\n", "")
+        contents = line.split(':')
+        (red, green, blue) = parse(contents[1].strip())
+        if(red <= R_CUBES and green <= G_CUBES and blue <= B_CUBES):
+            tot += game_counter
+print(tot)
